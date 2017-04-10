c4dc7202865e9367ebc5286959f17bdcdf357531
adadesions <ada_dev@icloud.com>
2017-04-10 15:12:47 +0700

    [issue#3] Completed

---
+++ include/adaGabor.hpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  adaGabor.hpp
+ *
+ *    Description:  header file for Gabor wavelet
+ *
+ *        Version:  1.0
+ *        Created:  04/07/2017 17:10:57
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Ada Kaminkure (AdaCode), ada@adacode.io
+ *        Company:  ADACODE.IO
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "opencv2/opencv.hpp";
+
+
--- include/adaVision.hpp
+++ include/adaVision.hpp
@@ -22,49 +22,46 @@
 #include <algorithm>
 #include "opencv2/opencv.hpp"
 
-void helloAda()
-{
-	std::cout << "Hello, Ada" << std::endl;
-}
-
-cv::Mat saltPepperNoise(cv::Mat input, int resolution)
+cv::Mat saltPepperNoise( cv::Mat input, int resolution )
 {
 	double prob = 0;
 	int res = resolution;
-	cv::Mat noise = input.clone();
-	for(int i=0; i<noise.rows; i += res)
+	cv::Mat noiseImg = input.clone();
+	for(int i=0; i<noiseImg.rows; i += res)
 	{
-		for(int j=0; j<noise.cols; j += res)
+		for(int j=0; j<noiseImg.cols; j += res)
 		{
 			prob = (rand()%11)*0.1;
 			if(prob < 0.1)
-				noise.at<uchar>(i,j) = 0;
+				noiseImg.at<uchar>(i,j) = 0;
 			else if(prob > 0.9)
-				noise.at<uchar>(i,j) = 255;
+				noiseImg.at<uchar>(i,j) = 255;
 		}
 	}
-	return noise;
+	return noiseImg;
 }
 
-int calSobel( cv::Mat input, int cx, int cy, char direction)
+int calSobel( cv::Mat input, int cx, int cy, char direction )
 {
 	//Sobel
 	int xMask[3][3] = {{1, 0, -1}, {2, 0, -2}, {1, 0, -1}};
 	int yMask[3][3] = {{1, 2, 1}, {0 ,0 ,0}, {-1, -2, -1}};
+
 	//Scharr
 	int xSchMask[3][3] = {{-3, 0, 3}, {-10, 0, 10}, {-3, 0, 3}};
 	int ySchMask[3][3] = {{-3, -10, -3}, {0, 0, 0}, {3, 10, 3}};
+
 	int ksize = 1;
-	int convoluted = 0;
+	int convolute = 0;
 	for(int i=-ksize; i<=ksize; i++)
 	{
 		for(int j=-ksize; j<=ksize; j++)
 		{
-			int m,n,x,y,mask;
+			int m, n, mask, rows, cols;
 			m = ksize+i;
 			n = ksize+j;
-			x = cx+i;
-			y = cy+j;
+			cols = cx+i;
+			rows = cy+j;
 			mask = 0;
 			if( x < 0 || y < 0)
 				continue;
@@ -76,10 +73,10 @@ int calSobel( cv::Mat input, int cx, int cy, char direction)
 			{
 				mask = yMask[m][n];
 			}
-			convoluted += (int)input.at<uchar>(y,x)*mask;
+			convolute += (int)input.at<uchar>(rows, cols) * mask;
 		}
 	}
-	return abs( convoluted );
+	return abs( convolute );
 }
 
 cv::Mat sobel( cv::Mat input )
@@ -92,6 +89,7 @@ cv::Mat sobel( cv::Mat input )
 	cv::Mat sobely(rows, cols, type);
 	cv::Mat sobelxy(rows, cols, type);
 	cv::Mat sobelAngle(rows, cols, type);
+
 	for(int i=0; i<rows; i++)
 	{
 		for(int j=0; j<cols; j++)
@@ -101,39 +99,14 @@ cv::Mat sobel( cv::Mat input )
 			uchar G = (uchar)std::sqrt(gx*gx + gy*gy);
 			double theta = std::atan((double)gy/(double)gx)*180/M_PI;
 
-			sobelx.at<uchar>(j,i) = gx; 
+			sobelx.at<uchar>(j,i) = gx;
 			sobely.at<uchar>(j,i) = gy;
 			sobelxy.at<uchar>(j,i) = G;
 			sobelAngle.at<uchar>(j,i) = (uchar)theta;
-		}	
-	}
-	cv::imshow("Gx", sobelx);
-	cv::imshow("Gy", sobely);
-	cv::imshow("Angle", sobelAngle);
-
-	//3 Channel
-	cv::Mat img_rgb(sobelAngle.size(), CV_8UC3);
-	cv::cvtColor(sobelAngle, img_rgb, CV_GRAY2RGB);
-	cv::Vec3b blue(255,0,0);
-	cv::Vec3b green(0,255,0);
-	cv::Vec3b red(0,0,255);
-	for(int i=0; i<img_rgb.rows; i++)
-	{
-		for(int j=0; j<img_rgb.cols; j++)
-		{
-			int intensity = img_rgb.at<cv::Vec3b>(j,i)[0];
-			if(intensity >=0 && intensity < 30)
-				img_rgb.at<cv::Vec3b>(j,i) = blue;
-			else if(intensity >=31 && intensity < 60)
-				img_rgb.at<cv::Vec3b>(j,i) = green;
-			else if(intensity >=61 && intensity < 90)
-				img_rgb.at<cv::Vec3b>(j,i) = red;
-				
 		}
 	}
-	cv::imshow("RGB Angle", img_rgb);
 	return sobelxy;
-}	
+}
 
 int calLocalAverage(cv::Mat input, int cx, int cy, int kernelSize)
 {
@@ -164,7 +137,7 @@ cv::Mat localAverage(cv::Mat input, int kernelSize)
 			meanValue = calLocalAverage(input, i, j, kernelSize);
 			result.at<uchar>(j, i) = (uchar)meanValue;
 		}
-	}	
+	}
 	return result;
 }
 
@@ -199,12 +172,7 @@ cv::Mat medianFilter(cv::Mat input, int kernelSize)
 		{
 			result.at<uchar>(j,i) = calMedianFilter(input, i, j, kernelSize);
 		}
-		cv::imshow("median step", result);
-		cv::waitKey(1);
 	}
 
 	return result;
 }
-
-
-
